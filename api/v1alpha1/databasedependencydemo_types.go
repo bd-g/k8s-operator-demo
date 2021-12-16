54e128524216ab79e89106ad5d0f4b1b6196ff5e
bgeorge <brent.george@amadeus.com>
2021-12-16 12:17:43 -0700

    after create api

---
+++ api/v1alpha1/databasedependencydemo_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DatabaseDependencyDemoSpec defines the desired state of DatabaseDependencyDemo
+type DatabaseDependencyDemoSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of DatabaseDependencyDemo. Edit databasedependencydemo_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// DatabaseDependencyDemoStatus defines the observed state of DatabaseDependencyDemo
+type DatabaseDependencyDemoStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// DatabaseDependencyDemo is the Schema for the databasedependencydemoes API
+type DatabaseDependencyDemo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DatabaseDependencyDemoSpec   `json:"spec,omitempty"`
+	Status DatabaseDependencyDemoStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// DatabaseDependencyDemoList contains a list of DatabaseDependencyDemo
+type DatabaseDependencyDemoList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DatabaseDependencyDemo `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DatabaseDependencyDemo{}, &DatabaseDependencyDemoList{})
+}
